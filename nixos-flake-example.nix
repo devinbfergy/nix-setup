8c9ec4d8c5d434617dfc28b5585f0252e7628858
devinbfergy <devin.ferguson.it@gmail.com>
2025-12-11 00:00:39 -0600

    Once again updating here

--- nixos-flake-example.nix
+++ nixos-flake-example.nix
@@ -3,17 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
+          nixos-wsl.nixosModules.wsl
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
